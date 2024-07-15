370d1eb8d0d6d0a49944ae7338035f6c309a9b18
Sowham Bhuin <sb2517@it.jgec.ac.in>
2024-07-15 14:21:05 +0530

    update

--- src/app.services.ts
+++ src/app.services.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hey There! Thanks for visiting my Todo REST API site!';
+    return 'Hey There! Sowham here , Thanks for visiting my rest API site';
   }
 }
